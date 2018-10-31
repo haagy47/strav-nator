4ba28c5d7a37d025340f241d8817ce3e03cf67aa
Emily Haag <emilyannehaag@gmail.com>
2018-10-31 16:19:34 -0600

    Deploying to prod

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
         'clientID'      : '29657',
         'clientSecret'  : '07eed663ab5101f09d3bc70a994a9f40c7c4b542',
         'accessToken'   : '63d072c5b2624b5c4159d8802f0dbde407a818ae',
-        'callbackURL'   : 'https://localhost:3000/auth/strava/callback'
+        'callbackURL'   : 'http://haagy47-strav-nator.herokuapp.com/auth/strava/callback'
     }
 };
